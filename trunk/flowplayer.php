a10f381b32be188c13bc32f952b5da174a42f699
Oneiroi <d.busby@saiweb.co.uk>
2009-01-13 16:58:24 +0000

    attempted fix at #15
    git-svn-id: http://svn.saiweb.co.uk/branches/wordpress-flowplayer@124 26e0a314-53de-4590-a415-dc075c606953

--- trunk/flowplayer.php
+++ trunk/flowplayer.php
@@ -350,7 +350,7 @@ class flowplayer
 	 */
 	public function __construct() {
 		//set conf path
-		$this->conf_path = $_SERVER['DOCUMENT_ROOT'].flowplayer::RELATIVE_PATH.'/saiweb_wpfp.conf';
+		$this->conf_path = getcwd().'/saiweb_wpfp.conf';
 		//if a post event has occured
 		if(isset($_POST['submit'])) {
 			//write config
