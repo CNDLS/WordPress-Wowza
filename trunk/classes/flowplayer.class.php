add6d3c2a3a42ca1e25b1a605f3ff29018f5f3c5
Oneiroi <d.busby@saiweb.co.uk>
2009-07-07 09:15:34 +0000

    2.0.9.9, updated readme.txt
    git-svn-id: http://svn.saiweb.co.uk/branches/wordpress-flowplayer@401 26e0a314-53de-4590-a415-dc075c606953

--- trunk/classes/flowplayer.class.php
+++ trunk/classes/flowplayer.class.php
@@ -35,14 +35,14 @@ class flowplayer {
 	}
 	function commercial_url(){
 		if(!defined('FLOWPLAYER_COMMERCIAL')){
-			define('FLOWPLAYER_COMMERCIAL',flowplayer::plugin_url().'/flowplayer/commercial/flowplayer.commercial-3.1.0-dev3.swf');
+			define('FLOWPLAYER_COMMERCIAL',flowplayer::plugin_url().'/flowplayer/commercial/flowplayer.commercial-3.1.1.swf');
 		}
 		return FLOWPLAYER_COMMERCIAL;
 	}
 	
 	function gpl_url(){
 		if(!defined('FLOWPLAYER_GPL')){
-			define('FLOWPLAYER_GPL',flowplayer::plugin_url().'/flowplayer/gpl/flowplayer-3.1.0-dev3.swf');
+			define('FLOWPLAYER_GPL',flowplayer::plugin_url().'/flowplayer/gpl/flowplayer-3.1.1.swf');
 		}
 		
 		return FLOWPLAYER_GPL;
@@ -108,7 +108,8 @@ class flowplayer {
 	function player_head(){
 		if(!defined('FLOWPLAYER_HEAD')){
 			$html = "\n<!-- Saiweb.co.uk Flowplayer For Wordpress Javascript Start -->\n";
-			$html .= '<script type="text/javascript" src="'.flowplayer::plugin_url().'/flowplayer/flowplayer-3.1.0-dev.min.js"></script>';
+			$html .= "\n".'<script type="text/javascript">WPFP = jQuery.noConflict(); $ = WPFP; </script>';
+			$html .= '<script type="text/javascript" src="'.flowplayer::plugin_url().'/flowplayer/flowplayer-3.1.1.min.js"></script>';
  			$html .= "\n<!-- Saiweb.co.uk Flowplayer For Wordpress Javascript END -->\n";
 			define('FLOWPLAYER_HEAD', $html);	
 		}		
@@ -118,7 +119,8 @@ class flowplayer {
 	function admin_head(){
 		if(!defined('FLOWPLAYER_ADMIN_HEAD')){
 			$html = "\n<!-- Saiweb.co.uk Flowplayer For Wordpress ADMIN Javascript Start -->\n";
- 			$html .= '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>';	
+ 			//$html .= '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>';	
+ 			$html .= "\n".'<script type="text/javascript">WPFP = jQuery.noConflict(); $ = WPFP; </script>';
  			$html .= "\n".'<script type="text/javascript" src="'.flowplayer::plugin_url().'/js/farbtastic/farbtastic.js"></script>';
  			$html .= "\n".'<script src="http://svn.saiweb.co.uk/branches/jquery_plugin/tags/latest/jquery.saiweb.min.js" type="text/javascript"></script>';
  			$html .= "\n".'<link rel="stylesheet" href="'.flowplayer::plugin_url().'/js/farbtastic/farbtastic.css" type="text/css" />';
@@ -182,10 +184,9 @@ class flowplayer {
 	function flowplayer_settings(){
 		$html = '
 					<script language="Javascript" type="text/javascript">
-	$(document).ready(function() {
+	WPFP(document).ready(function() {
 		//load player
 		$f("player", "'.(flowplayer::_getkey()?flowplayer::commercial_url():flowplayer::gpl_url()).'", {
-				'.(flowplayer::_getkey()?'key:\''.flowplayer::_getkey().'\',':'').'
 				plugins: {
   					 controls: {    					
       					buttonOverColor: \''.flowplayer::_getbuttonOverColor().'\',
@@ -207,7 +208,8 @@ class flowplayer {
 					url:\'http://saiweb.co.uk/wp-content/videos/wpfp_config_demo.mp4\',
 					autoPlay: '.(flowplayer::_getautoplay()=='true'?'true':'false').',
        				autoBuffering: '.(flowplayer::_getautobuffer()=='true'?'true':'false').'
-				},';
+				},
+				'.(flowplayer::_getkey()?'key:\''.flowplayer::_getkey().'\',':'');
 if($fp->conf['logoenable'] == 'true'){
 	$html .= '
 		logo: {  
@@ -229,7 +231,7 @@ $html .= '
 				}
 			});
 		//colour picker call back to api and hidden vars		
-		$(\'#colourpicker\').farbtastic(function(color){
+		WPFP(\'#colourpicker\').farbtastic(function(color){
 			var tgt = $(":input[name=tgt]:checked").val();
 			tgtArr = tgt.split(\'_\');
 			//set to hidden input
@@ -258,10 +260,16 @@ $html .= '
 						<form id="wpfp_options" method="post">
 						<div id="icon-options-general" class="icon32"><br></div>
 						<h2><a href="http://www.saiweb.co.uk">Saiweb</a> Flowplayer for Wordpress</h2>
+						';
+						if(!flowplayer::_getkey()){
+							$html .= '
 						<h3>Like this plugin?</h3>
 							A lot of development time and effort went into Flowplayer and this plugin, you can help support further development by purchasing a comercial license for flowplayer.
 						<h3><a href="http://flowplayer.org/download/index.html?aff=100&src=saiweb_plugin&domain='.$_SERVER['SERVER_NAME'].'" target="_blank">Get a commercial license now!</a></h3>
-						<h3>Please set your default player options below</h3>
+						';
+						
+						}
+						$html .= '<h3>Please set your default player options below</h3>
 						<table>
 							<tr>
 								<td>AutoPlay: </td>
@@ -306,9 +314,83 @@ $html .= '
 		return $html;
 	}
 	
-	function build_player(){
-		global $post;
+	 /** 
+	  * Salt function
+	  * @see http://trac.saiweb.co.uk/saiweb/ticket/2
+	  * @return string salt
+	  */
+	function _salt() {
+        $salt = substr(md5(uniqid(rand(), true)), 0, 10);    
+        return $salt;
+	}
+	
+	function legacy_hook($content){
 		
+		$regex = '/\[FLOWPLAYER=([a-z0-9\:\.\-\&\_\/]+)\,([0-9]+)\,([0-9]+)\]/i';
+		$matches = array();
+	
+		preg_match_all($regex, $content, $matches);
+		
+		if($matches[0][0] != '') {
+			foreach($matches[0] as $key => $data) {
+				$content = str_replace($matches[0][$key], flowplayer::build_player($matches[2][$key], $matches[3][$key], $matches[1][$key]),$content);
+			}	
+		}
+		
+		return $content;
+	}
+	
+	function build_player($width,$height,$media){
+			/*
+			 * Fix #2 
+			 * @see http://trac.saiweb.co.uk/saiweb/ticket/2
+			 */
+			$hash = md5($media.flowplayer::_salt());
+			$html = '<div id="saiweb_'.$hash.'" style="width:'.$width.'px; height:'.$height.'px;"></div>';
+			$html .= '<script language="Javascript" type="text/javascript">
+	WPFP(document).ready(function() {
+		//load player
+		$f("saiweb_'.$hash.'", "'.(flowplayer::_getkey()?flowplayer::commercial_url():flowplayer::gpl_url()).'", {
+				plugins: {
+  					 controls: {    					
+      					buttonOverColor: \''.flowplayer::_getbuttonOverColor().'\',
+      					sliderColor: \''.flowplayer::_getsliderColor().'\',
+      					bufferColor: \''.flowplayer::_getbufferColor().'\',
+      					sliderGradient: \'none\',
+      					progressGradient: \'medium\',
+      					durationColor: \''.flowplayer::_getdurationColor().'\',
+      					progressColor: \''.flowplayer::_getprogressColor().'\',
+      					backgroundColor: \''.flowplayer::_getbackgroundColor().'\',
+      					timeColor: \''.flowplayer::_gettimeColor().'\',
+      					buttonColor: \''.flowplayer::_getbuttonColor().'\',
+      					backgroundGradient: \'none\',
+      					bufferGradient: \'none\',
+   						opacity:1.0
+   						}
+				},
+				clip: {
+					url:\''.$media.'\',
+					autoPlay: '.(flowplayer::_getautoplay()=='true'?'true':'false').',
+       				autoBuffering: '.(flowplayer::_getautobuffer()=='true'?'true':'false').'
+				},
+				'.(flowplayer::_getkey()?'key:\''.flowplayer::_getkey().'\',':'');
+/*if($fp->conf['logoenable'] == 'true'){
+	$html .= '
+		logo: {  
+        url: \'http://'.$fp->conf['logo'].'\',  
+        fullscreenOnly: '.$fp->conf['fullscreenonly'].',  
+        displayTime: 0,
+        linkUrl: \'http://'.$fp->conf['logolink'].'\' 
+    },';
+}*/
+$html .= '
+				canvas: {
+					backgroundColor:\''.flowplayer::_getcanvas().'\'
+				}})
+			});</script>
+				';
+				
+		return $html;	
 	}
 }
 
--- trunk/readme.txt
+++ trunk/readme.txt
@@ -3,8 +3,8 @@ Contributors: d.busby
 Donate link: http://www.saiweb.co.uk/wordpress-flowplayer#donate
 Tags: flowplayer, mp4, flv, h264, libx264, HD, streaming, flow, player
 Requires at least: 2.7
-Tested up to: 2.7
-Stable tag: 2.0.1.4
+Tested up to: 2.8
+Stable tag: 2.0.9.9
 
 A Flowplayer plugin for wordpress.
 
@@ -12,9 +12,9 @@ A Flowplayer plugin for wordpress.
 
 Homepage: http://www.saiweb.co.uk/wordpress-flowplayer
 
-NOTE: IF YOU ARE UPGRADING REMEMBER TO BACKUP YOUR /wp-content/plugins/word-press-flow-player/saiweb_wpfp.conf
+NOTE: /wp-content/plugins/word-press-flow-player/saiweb_wpfp.conf has now been removed, Meta data is now in use, you will have to re-enter your settings!
 
-The version of flowplayer provided with this plugin is version 3.0.3 and is provided under the GPL license as specified by the authors of flowplayer http://flowplayer.org/.
+The version of flowplayer provided with this plugin is version 3.1.1 and is provided under the GPL license as specified by the authors of flowplayer http://flowplayer.org/.
 
 As of version 2.0.1.3, the bundeled version is the commercial version of flowplayer distributed with permission from the authors, this will allows owners of a commercial key to access commercial features.
 
@@ -35,18 +35,19 @@ This section describes how to install the plugin and get it working.
 
 e.g.
 
-1. Download plugin and upload all contents to `/wp-content/plugins/`
-2. Create a directory in your webroot for media files `/wp-content/videos/`
-3. Activate the plugin through the 'Plugins' menu in WordPress
-4. Place [FLOWPLAYER=media.flv,600,450] where media.flv is your media file in `/wp-content/videos/` in your blog text (As of 2.0.1.0 you can now include media via http i.e. [FLOWPLAYER=http://another-site.com/media.flv,600,450]).
-5. Updates are made available automatically via the wordpress upgrade system, please ensure you read the change log before upgrading.
-6. You can make changes to the default settings by visiting the configuration page i.e. http://your_website_here/wp-admin/options-general.php?page=flowplayer.php
+1. Download plugin and upload all contents to `/wp-content/plugins/``
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place [FLOWPLAYER=media.flv,600,450] where media.flv is your media file path, this can be realative or a full http url, followed by the width,height.
+4. Updates are made available automatically via the wordpress upgrade system, please ensure you read the change log before upgrading.
+5. You can make changes to the default settings by visiting the configuration page i.e. http://your_website_here/wp-admin/options-general.php?page=flowplayer.php
 
 == Frequently Asked Questions ==
 
 = I get an error: Parse error: parse error, unexpected T_STRING, expecting T_OLD_FUNCTION or T_FUNCTION or T_VAR or } in /wp-content/plugins/word-press-flow-player/wpfp/flowplayer.php on line XX =
 
 This plugin is incompatible with releases 4.x of PHP, please update your PHP installation to 5.x.
+
+NOTE: versions 2.0.9.9 _MAY_ be compatible with php 4.x this is untested at this time.
 
 = I have another error / problem / question / suggestion =
 
